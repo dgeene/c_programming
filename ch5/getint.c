34ea0f34fc9cf6deae746ba1d0976aa9d5142010
dgeene <david.geene@gmail.com>
2015-12-08 11:29:40 -0500

    need to clean up getint.

--- ch5/getint.c
+++ ch5/getint.c
@@ -33,21 +33,30 @@ int getint(int *pn)
 {
     int c, sign;
 
-
     while ( isspace(c = getch()) ) // skip white space
         ;
+
 
     if ( !isdigit(c) && c != EOF && c != '+' && c != '-' )
     {
         ungetch(c); // its NaN
         return 0;
     }
+
+
+
     sign = (c == '-') ? -1 : 1;
     if( c == '+' || c == '-' )
         c = getch();
 
-    for ( *pn = 0; isdigit(c); c = getch() ) //what?
+
+
+    for ( *pn = 0; isdigit(c); c = getch() )
+    {
         *pn = 10 * *pn + (c - '0');
+    }
+
+
     *pn *= sign;
     if ( c != EOF )
         ungetch(c);
