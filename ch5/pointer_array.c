db46ededd50562aa25965a5705300e44a225d570
dgeene <david.geene@gmail.com>
2015-12-08 11:51:10 -0500

    playing with arrays

---
+++ ch5/pointer_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    a[0] = 45;
+    a[1] = 66;
+
+    int *pa;
+    pa = &a[0]; // pa points to 0th element of a
+
+
+    int x;
+    x = *pa; // copy contents of a[0] into x
+
+    printf("a[0] is: %d\n", a[0]);
+    printf("%p\n", pa); //prints pointer address
+    printf("x is: %d\n", x);
+    printf("pa+1 is: %p\n", pa+1); //so pa+1 points to the next ADDRESS OF a? so the address of a[1]?
+    printf("*(pa+1) is: %d\n", *(pa+1)); //this prints the actual value, do 66
+}
