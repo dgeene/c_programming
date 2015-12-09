0994d14759aca71e52df3994a4566090c8dad2a1
dgeene <david.geene@gmail.com>
2015-12-08 19:14:37 -0500

    add notes about pointers

--- ch5/pointer_array.c
+++ ch5/pointer_array.c
@@ -13,9 +13,12 @@ int main()
     int x;
     x = *pa; // copy contents of a[0] into x
 
-    printf("a[0] is: %d\n", a[0]);
+    printf("a[0] is: %d\n", a[0]); // 'a[0]' is actually *(a[0]) ?
     printf("%p\n", pa); //prints pointer address
     printf("x is: %d\n", x);
     printf("pa+1 is: %p\n", pa+1); //so pa+1 points to the next ADDRESS OF a? so the address of a[1]?
     printf("*(pa+1) is: %d\n", *(pa+1)); //this prints the actual value, do 66
+    printf("pa[1] is: %d\n", pa[1]); // automatically dereferences
+
+    printf("a: %d\n", a); // using just 'a' refers to the first elem. use '*a' to print actual value
 }
