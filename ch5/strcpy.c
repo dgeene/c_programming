ff143e93273b7eb8b6489ff0684da1cfd2fd9df4
Geene, David <david_geene@bose.com>
2015-12-15 11:40:36 -0500

    add ch5 examples

---
+++ ch5/strcpy.c
@@ -0,0 +1,42 @@
+#include <stdio.c>
+
+/*
+ * Section 5.5
+ * Illustrates aspects if pointers and arrays.
+ * To copy a string we cant just say t = s. That will only copy
+ * the pointer.
+ */
+
+
+
+// copy characters from t to s
+void strcpy( char *s, char *t )
+{
+    int i;
+
+    i = 0;
+    while ( (s[i] = t[i]) != '\0' )
+        i++;
+}
+
+// pointer version
+void strcpyptr( char *s, char *t )
+{
+    while ( (*s++ = *t++) != '\0' )
+        ;
+}
+
+
+
+/*
+ * Compare string s and t lexicographically.
+ * returns: <0 is s<t, 0 if s==t, >0 if s>t
+ * pointer version
+ */
+int strcmpptr( char *s, char *t )
+{
+    for ( ; *s == *t; s++, t++ )
+        if ( *s == '\0' )
+            return 0;
+    return *s - *t;
+}
---
+++ ch5/strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * Section 5.4 address arithmetic
+ */
+
+
+
+/*
+ * return length of string s
+ */
+int strlen( char *s )
+{
+    char *p = s; // point to the first charcater of string
+
+    while ( *p != '\0' ) //examine the string until the end is seen
+        p++;
+
+    return p - s; // give the num of characters advanced over
+}
