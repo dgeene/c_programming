797cb3fb648d0dd00e5093797994c0bce088d6a3
dgeene <david.geene@gmail.com>
2016-02-02 12:12:04 -0500

    additional changes

--- misc/memory_pool/memory_pool.c
+++ misc/memory_pool/memory_pool.c
@@ -24,13 +24,68 @@ MEMORYPOOL allocNode;
 // init the memory pool
 void initializeMemoryPool()
 {
-    unsigned char uIndx;
-
-    for (uIndx = 0; uIndx < MAX_MEMORY_POOL_NODES; uIndx++)
+    unsigned char ii;
+    for (ii = 0; ii < MAX_MEMORY_POOL_NODES; ii++)
     {
-        allocNode.array[uIndx] = (NODE *)malloc(sizeof(NODE));
-        allocNode.array[uIndx]->nodeAllocated = 0;
+        allocNode.array[ii] = (NODE *)malloc(sizeof(NODE));
+        allocNode.array[ii]->nodeAllocated = 0;
         allocNode.nodeCounter++;
+    }
+}
+
+
+/*
+ * get memory from the pool of nodes
+ */
+NODE *getFreeNodeFromPool()
+{
+    int ii;
+
+    for (ii = 0; ii < MAX_MEMORY_POOL_NODES; ii++)
+    {
+        if (allocNode.array[ii]->nodeAllocated == 0)
+        {
+            allocNode.array[ii]->nodeAllocated = 1;
+            allocNode.nodeCounter--;
+            break;
+        }
+    }
+    if (ii == MAX_MEMORY_POOL_NODES)
+    {
+        printf("No free nodes available.\n");
+        return NULL;
+    }
+    return allocNode.array[ii];
+}
+
+
+
+/*
+ * Return a node to the memory pool
+ */
+void addNodeToMemoryPool(unsigned char i)
+{
+    if (allocNode.array[i]->nodeAllocated ==1)
+    {
+        allocNode.array[i]->data = 0;
+        allocNode.array[i]->nodeAllocated = 0;
+        allocNode.nodeCounter++;
+    } else {
+        printf("No nodes to add.\n");
+        return;
+    }
+}
+
+
+/*
+ * Deallocate the memory pool
+ */
+void deallocatePool()
+{
+    unsigned char ii;
+    for (ii = 0; ii < MAX_MEMORY_POOL_NODES; ii++)
+    {
+        free(allocNode.array[ii]);
     }
 }
 
@@ -38,18 +93,36 @@ void initializeMemoryPool()
 
 int main()
 {
+    unsigned char uindx, ii;
 
     printf("size node: %d\n", sizeof(NODE));
     printf("size mem pool: %d\n", sizeof(MEMORYPOOL));
     printf("size alloc node: %d\n", sizeof(allocNode));
+
     initializeMemoryPool();
     printf("size alloc node after init: %d\n", sizeof(allocNode));
+
+
+    // get the node from the memory pool
+    for (uindx = 0; uindx < MAX_MEMORY_POOL_NODES; uindx++)
+    {
+        NODE *node = NULL;
+        int data;
+
+        node = getFreeNodeFroPool();
+
+        if (node)
+        {
+            printf("Enter the Data to be entered in the LinkList \n");
+            scanf("%d", &data);
+
+            node->data = data;
+        }
+
+        // display data
+    }
+
+
+
     return 0;
-}
-
-
-
-NODE *getFreeNodeFromPool()
-{
-    int i;
 }
